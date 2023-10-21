f32646104eec0d2439d749d6dc4f26aeb3933256
j1cobs <jacosse1@gmail.com>
2023-10-21 18:19:01 -0400

    Add exe with voices folder

--- file_to_mp3.py
+++ file_to_mp3.py
@@ -6,6 +6,7 @@ from pptx import Presentation
 import docx
 import os
 import functions
+import Voices
 
 
 
@@ -126,3 +127,13 @@ root.withdraw()
 messagebox.showinfo("Final Step","It's all done, have a good day!")
 root.destroy()
 
+# Function to use the voices folder in the exe file
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
